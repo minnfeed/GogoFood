8ee485590d115d354772087e96cee09c1811f385
Lê Duy Khang <lehoanggiakhuong2006@gmail.com>
2025-06-14 10:53:34 +0700

    New project

---
+++ backend/springboot-services/user-service/src/test/java/com/example/springbootservices/SpringbootServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
